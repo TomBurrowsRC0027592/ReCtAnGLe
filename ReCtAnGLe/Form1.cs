c99db14ec7f322d88f0a57fab0c742a23e76377c
Tom Burrows <rc0027592@student.riversidecollege.ac.uk>
2018-12-03 15:44:42 +0000

    added int  for the labelled text boxes to make it work

--- ReCtAnGLe/Form1.cs
+++ ReCtAnGLe/Form1.cs
@@ -16,5 +16,14 @@ namespace ReCtAnGLe
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int w, h;
+            w = int.Parse(textBox1.Text);
+            h = int.Parse(textBox2.Text);
+            int area = h * w;
+            textBox3.Text = area.ToString();
+        }
     }
 }
